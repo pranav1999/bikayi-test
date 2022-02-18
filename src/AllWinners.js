ad272d4ca5463eedbf509e02abaf3776b0b8f7a0
pranav kumar soni <pranav.kumarsoni1999@gmail.com>
2022-02-19 04:37:24 +0530

    prefinal

--- src/AllWinners.js
+++ src/AllWinners.js
@@ -3,39 +3,65 @@ import Prize from "./Prize";
 import "./AllWinners.css";
 
 function AllWinners() {
-
   const API = "http://api.nobelprize.org/v1/prize.json";
   const [Data, setData] = useState([]);
 
   useEffect(() => {
     const get = async () => {
-      const res = await fetch(API); 
+      const res = await fetch(API);
       const d = await res.json();
       const s = d.prizes;
       setData(s);
-      console.log(s); //  
+      console.log(s); //
     };
     get();
   }, []);
 
+  var prizeyear = [];
+  for(let i=1900;i<=2021;i++){
+    prizeyear.push(i);
+  }
+
+  var domains = ["chemistry","economics","litrature","peace","physics","medicine"]
 
   return (
     <div className="allWinners">
       <h1>All Winners</h1>
       <div className="allWinner-data">
-        {Data.length>0 && Data.map(({year,category,laureates})=>{
-          return(
-            <div>
-            <Prize
-            key={year+category}
-            year = {year}
-            category = {category}
-            motivation = "We will find some motivation and will always win this game."
-            arr = {laureates!==undefined && laureates} 
-            />
-            </div>
-          )
-        })}
+        <div className="filters">
+          <select name="year" id="prizeyear">
+              <option value="">Choose Year</option>
+              {
+                prizeyear.map((data)=>{
+                  return(
+                    <option key={data} value={data}>{data}</option>
+                  )
+                })
+              }
+          </select>
+          <select name="domain" id="prizedomain">
+              <option value="">Choose Domain</option>
+              {
+                domains.map((data)=>{
+                  return(
+                    <option key={data} value={data}>{data}</option>
+                  )
+                })
+              }
+          </select>
+        </div>
+        {Data.length > 0 &&
+          Data.map(({ year, category, overallMotivation, laureates }) => {
+            return (
+              <Prize
+                key={year + category}
+                year={year}
+                category={category}
+                motivation={overallMotivation}
+                arr={laureates !== undefined && laureates}
+              />
+            );
+          })}
       </div>
     </div>
   );
--- src/Prize.js
+++ src/Prize.js
@@ -8,14 +8,17 @@ function Prize(props) {
     <div className="prize">
       <div className="prize-top">
         <h4>{props.year}</h4>
-        <h4>{props.category}</h4>
+        <h4>{props.category.toUpperCase()}</h4>
       </div>
       <p id="motivation">{props.motivation}</p>
       <div className="laurates">
         {props.arr.length>0 &&
           props.arr.map((data) =>{
             return(
-              <p>{data.firstname}<span>{data.surname}</span></p>
+              <div key={data.id}>
+              <p id="name">{data.firstname} <span>{data.surname}</span></p>
+              <p id="motivation">{data.motivation}</p>
+              </div>
             )
           })
         }
